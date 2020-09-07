a44b3f9f2a3d361bb22f5bb52c437bbbe9e6176e
Naam <naam.pt@gmail.com>
2020-09-07 09:09:19 -0400

    add search icon and make card component clickable

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/App.css';
 import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ChatPage from './components/ChatPage';
 
-function App() {
-  return (
-    <div className='App'>
-      {/* <Login /> */}
-      <ChatPage />
-    </div>
-  );
-}
+const App = () => {
+  const [isLoggedin, setIsLoggedIn] = useState(true);
+
+  const displayPage = isLoggedin ? <ChatPage /> : <Login />;
+
+  return <div className='App'>{displayPage}</div>;
+};
 
 export default App;
--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -2,23 +2,36 @@ import React from 'react';
 import { Card, Container, InputGroup, FormControl } from 'react-bootstrap';
 import './ChatPage.css';
 import userLogo from '../images/mock-user-image.png';
+import { FaSearch } from 'react-icons/fa';
+// import { Link } from 'react-router-dom';
 
 const chat = () => {
   return (
     <Container className='mt-5'>
       <div className='contacts-container'>
         <p className='text-left'>Chat</p>
-        <InputGroup className='mb-3'>
+
+        <InputGroup className='mb-3 input-wrapper'>
           <FormControl
             placeholder='Search Chat'
             aria-label='Amount (to the nearest dollar)'
           />
+          <InputGroup.Append>
+            <InputGroup.Text>
+              <FaSearch color='grey' className='search-icon' />
+            </InputGroup.Text>
+          </InputGroup.Append>
         </InputGroup>
+
         <Card>
-          <Card className='d-flex flex-row mx-auto'>
-            <Card.Img variant='left' src={userLogo} className='user-logo ' />
-            <Card body>some text within a card body.</Card>
-          </Card>
+          {/* <Link to='/batch/list'> */}
+          <div onClick={() => alert('Hello from here')} className='mx-auto'>
+            <Card className='d-flex flex-row card-hoverable' hoverable>
+              <Card.Img variant='left' src={userLogo} className='user-logo ' />
+              <Card body>some text within a card body.</Card>
+            </Card>
+          </div>
+          {/* </Link> */}
           <Card body>This is some text within a card body.</Card>
           <Card body>This is some text within a card body.</Card>
         </Card>
@@ -28,3 +41,7 @@ const chat = () => {
 };
 
 export default chat;
+
+//link
+//
+//https://stackoverflow.com/questions/19285640/font-awesome-icon-inside-text-input-element
